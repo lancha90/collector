54297b3d63323756949e9f850160194f75e937f9
diego mauricio herrera alzate <diegomao627@gmail.com>
2015-11-14 17:43:55 +0000

    url

--- app/src/main/java/colector/co/com/collector/http/ResourceNetwork.java
+++ app/src/main/java/colector/co/com/collector/http/ResourceNetwork.java
@@ -5,9 +5,9 @@ public class ResourceNetwork {
 	private static final String URL_BASE_LOCAL = "http://192.168.30.232/";
 
 	private static final String URL_BASE = "http://52.88.209.73:9000/";
-//	public static final String URL_LOGIN = URL_BASE+"authentication/onestep/";
+	public static final String URL_LOGIN = URL_BASE+"authentication/onestep/";
 	public static final String URL_SURVEY = URL_BASE+"service/form/all/";
-	public static final String URL_SURVEY = "http://192.168.1.118/json.json";
+//	public static final String URL_SURVEY = "http://192.168.1.118/json.json";
 	public static final String URL_SEND_SURVEY = URL_BASE+"service/fill/responses/";
 
 }
